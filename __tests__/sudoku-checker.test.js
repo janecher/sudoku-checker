b7832012f89aa94cb083c8c54026c4a69f1f8e9f
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-23 15:46:33 -0700

    add tests to check all horizontal and vertical lines follow the sudoku rules
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- __tests__/sudoku-checker.test.js
+++ __tests__/sudoku-checker.test.js
@@ -1,4 +1,4 @@
-import { horizontalCheck } from './../src/main.js';
+import { horizontalCheck, allHorizontalLinesCheck, verticalCheck, allVerticalLinesCheck } from './../src/main.js';
 
 describe('horizontalCheck', () => {
 
@@ -7,5 +7,36 @@ describe('horizontalCheck', () => {
     const array2 = [2, 3, 3, 1];
     expect(horizontalCheck(array1)).toEqual(true);
     expect(horizontalCheck(array2)).toEqual(false);
+  });
+});
+
+describe('allHorizontalLinesCheck', () => {
+
+  test('should check if all horizontal arrays pass sudoku rules', () => {
+    const array1 = [[1, 2, 4, 3], [4, 3, 1, 2], [3, 4, 2, 1], [2, 1, 3, 4]];
+    const array2 = [[1, 2, 4, 3], [4, 3, 3, 2], [3, 4, 2, 1], [2, 1, 3, 4]];
+    expect(allHorizontalLinesCheck(array1)).toEqual(true);
+    expect(allHorizontalLinesCheck(array2)).toEqual(false);
+  });
+});
+
+describe('verticalCheck', () => {
+
+  test('should check if all numbers in sudoku array of array at particular index are unique', () => {
+    const array1 = [[1, 2, 4, 3], [4, 3, 1, 2], [3, 4, 2, 1], [2, 1, 3, 4]];
+    const array2 = [[1, 2, 4, 3], [4, 3, 2, 2], [3, 4, 2, 1], [2, 1, 3, 4]];
+    const j = 2;
+    expect(verticalCheck(array1, j)).toEqual(true);
+    expect(verticalCheck(array2, j)).toEqual(false);
+  });
+});
+
+describe('allVerticalLinesCheck', () => {
+
+  test('should check if all vertical lines pass sudoku rules', () => {
+    const array1 = [[1, 2, 4, 3], [4, 3, 1, 2], [3, 4, 2, 1], [2, 1, 3, 4]];
+    const array2 = [[1, 2, 4, 3], [4, 3, 2, 2], [3, 4, 2, 1], [2, 1, 3, 4]];
+    expect(allVerticalLinesCheck(array1)).toEqual(true);
+    expect(allVerticalLinesCheck(array2)).toEqual(false);
   });
 });
