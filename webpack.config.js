4cb04630b8ff3e27ebcf65355bf14ec56e5b15ea
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-23 15:12:58 -0700

    change name of the project
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'project-name',
+      title: 'Sudoku Checker',
       template: './src/index.html',
       inject: 'body'
     })
