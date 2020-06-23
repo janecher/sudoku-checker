70f8ed965d94b0fd3376b1433d32718a2022f6ca
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-23 15:47:19 -0700

    add functions to check all horizontal and vertical lines follow the sudoku rules
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,11 +5,53 @@
 
 export function horizontalCheck(array) {
   let checkArray = [];
-  for(let i=0; i < array.length; i++){
-    if(checkArray.includes(array[i])){
+  for (let i=0; i < array.length; i++) {
+    if (checkArray.includes(array[i])) {
       return false;
     }
     checkArray.push(array[i]);
   }
   return true;
 }
+
+export function allHorizontalLinesCheck(sudokuArray) {
+  for (let i=0; i<sudokuArray.length; i++) {
+    if (!horizontalCheck(sudokuArray[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function verticalCheck(sudokuArray, j) {
+  let checkArray = [];
+  for (let i=0; i<sudokuArray.length; i++) {
+    if(checkArray.includes(sudokuArray[i][j])) {
+      return false;
+    }
+    checkArray.push(sudokuArray[i][j]);
+  }
+  return true;
+}
+
+export function allVerticalLinesCheck(sudokuArray) {
+  for (let i=0; i<sudokuArray.length; i++) {
+    if (!verticalCheck(sudokuArray, i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*sudokuArray = [[1, 2, 4, 3], [4, 3, 1, 2], [3, 4, 2, 1], [2, 1, 3, 4]]
+0 1 2 3
+
+j = 2
+
+sudokuArray[i][j]
+sudokuArray[0] = [1, 2, 4, 3]
+
+sudokuArray[0][2] = 4;
+sudokuArray[1][2] = 1;
+sudokuArray[2][2] = 2;
+sudokuArray[3][2] = 3;*/
